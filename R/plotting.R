641de42209143b7e2920b99d82533c34448ebb18
bio1122 <169012173+bio1122@users.noreply.github.com>
2026-02-25 17:00:21 +0800

    fix

--- R/plotting.R
+++ R/plotting.R
@@ -6,8 +6,8 @@ monocle_theme_opts <- function()
 {
     theme(strip.background = element_rect(colour = 'white', fill = 'white')) +
     theme(panel.border = element_blank()) +
-    theme(axis.line.x = element_line(size=0.25, color="black")) +
-    theme(axis.line.y = element_line(size=0.25, color="black")) +
+    theme(axis.line.x = element_line(linewidth=0.25, color="black")) +
+    theme(axis.line.y = element_line(linewidth=0.25, color="black")) +
     theme(panel.grid.minor.x = element_blank(), panel.grid.minor.y = element_blank()) +
     theme(panel.grid.major.x = element_blank(), panel.grid.major.y = element_blank()) + 
     theme(panel.background = element_rect(fill='white')) +
@@ -2377,8 +2377,8 @@ plot_complex_cell_trajectory <- function(cds,
     #scale_color_brewer(palette="Set1") +
     theme(strip.background = element_rect(colour = 'white', fill = 'white')) +
     theme(panel.border = element_blank()) +
-    # theme(axis.line.x = element_line(size=0.25, color="black")) +
-    # theme(axis.line.y = element_line(size=0.25, color="black")) +
+    # theme(axis.line.x = element_line(linewidth=0.25, color="black")) +
+    # theme(axis.line.y = element_line(linewidth=0.25, color="black")) +
     theme(panel.grid.minor.x = element_blank(), panel.grid.minor.y = element_blank()) +
     theme(panel.grid.major.x = element_blank(), panel.grid.major.y = element_blank()) + 
     theme(panel.background = element_rect(fill='white')) +
